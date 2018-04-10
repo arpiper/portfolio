03ac38e33237add4b1481a0dde5a1ac5794f0a89
Andrew Piper <pipera@uw.edu>
2018-04-10 16:20:58 -0700

    new build output

--- webpack.config.js
+++ webpack.config.js
@@ -59,6 +59,11 @@ module.exports = {
 if (process.env.NODE_ENV === 'production') {
   module.exports.devtool = '#source-map'
   // http://vue-loader.vuejs.org/en/workflow/production.html
+  module.exports.output = { 
+    path: '/home/arch/gcp/pipes/spa/portfolio',
+    publicPath: '/',
+    filename: 'build.js'
+  },
   module.exports.plugins = (module.exports.plugins || []).concat([
     new webpack.DefinePlugin({
       'process.env': {
